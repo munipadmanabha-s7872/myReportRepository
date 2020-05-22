c77393756fcff0b3711bc5a6c31d621822017af9
Sindhu <sindhumahi79@gmail.com>
2020-05-22 09:05:08 -0700

    Adding input and output to be shown in the java file

--- src/myFirstJava.java
+++ src/myFirstJava.java
@@ -19,7 +19,11 @@ public class myFirstJava {
 		String name;
 		int age;
 		Scanner input= new Scanner(System.in);
-		
+		System.out.print("Please enter your name:");
+		name=input.nextLine();
+		System.out.print("Please enter you age:");
+		age=input.nextInt();
+		System.out.println("Your name is "+name+" and you are "+ age+" years old ");
 	}
 
 }
